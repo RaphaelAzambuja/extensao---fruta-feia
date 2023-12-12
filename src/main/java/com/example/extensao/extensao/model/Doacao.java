d9d23b8e0f96bc6d40bd2fec3dce922c37902ccf
manuela-sventnickas <manu.svent@gmail.com>
2023-12-11 21:22:54 -0300

    commit

--- src/main/java/com/example/extensao/extensao/model/Doacao.java
+++ src/main/java/com/example/extensao/extensao/model/Doacao.java
@@ -3,8 +3,8 @@ package com.example.extensao.extensao.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-
 import java.time.LocalDate;
+
 @Entity
 public class Doacao extends EntityId {
     @Column(name = "tipo", nullable = false)
